2548da0fab8a4456d41dabf7a7d3c7fc02a0f671
409H <409H@users.noreply.github.com>
2020-02-10 03:20:09 +0000

    Release

---
+++ precache-manifest.dac11de343aae57d2948b5be5aafae05.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fa4f6dfdcf70fd5d7f6802fee11d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f7e363a55be93f22e36",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "473ba8a1a08ab112a06d",
+    "url": "/static/js/2.9f410fda.chunk.js"
+  },
+  {
+    "revision": "d872e49062d924d1ad0175d1cf7d9cd5",
+    "url": "/static/js/2.9f410fda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f7e363a55be93f22e36",
+    "url": "/static/js/main.3bcb37d5.chunk.js"
+  },
+  {
+    "revision": "116afb9cce933d69a460",
+    "url": "/static/js/runtime-main.c705a541.js"
+  },
+  {
+    "revision": "ad13ed90a4f50caf9a2600f4e1f9bd87",
+    "url": "/static/media/ambo.ad13ed90.png"
+  },
+  {
+    "revision": "b3a3bbc81369501f3055feb09ec34e27",
+    "url": "/static/media/aragon.b3a3bbc8.png"
+  },
+  {
+    "revision": "ed1bb33d546ae1e8af773dbe570504b6",
+    "url": "/static/media/ensportal-logo.ed1bb33d.png"
+  },
+  {
+    "revision": "972fdd21ff9f370952df89662da789c1",
+    "url": "/static/media/etherscan.972fdd21.png"
+  },
+  {
+    "revision": "023762b6aec2a2249b8fdfb638f00ef3",
+    "url": "/static/media/metamask.023762b6.png"
+  },
+  {
+    "revision": "5c3f08040d07a98d9399d72231e842a5",
+    "url": "/static/media/moon.5c3f0804.svg"
+  },
+  {
+    "revision": "e200825f7690cd7cf4a560e2ea5fada2",
+    "url": "/static/media/mycrypto.e200825f.png"
+  },
+  {
+    "revision": "88ef369a7d433755ee421a7adaa9f620",
+    "url": "/static/media/sun.88ef369a.svg"
+  },
+  {
+    "revision": "b62b0aa13669d1017bec56f850f0d673",
+    "url": "/static/media/truffle.b62b0aa1.png"
+  }
+]);
